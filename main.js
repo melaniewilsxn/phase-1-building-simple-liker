1e24b382a21c784211b23dc120fe27b9cdcfcbff
Melanie Wilson <melaniemwilson1@gmail.com>
2023-04-19 23:11:18 -0400

    completed lab

--- main.js
+++ main.js
@@ -3,9 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.getElementById('modal').className = 'hidden'
 
+const likeBtns = document.querySelectorAll('.like-glyph')
+  
+for (btn of likeBtns){
+  btn.addEventListener('click', handleClick)
+}
 
-
+function handleClick(e){
+  const heart = e.target
+  mimicServerCall()
+    .then(() => {
+      if (heart.textContent === '♡'){
+      heart.textContent = FULL_HEART
+      heart.className = 'activated-heart'
+      } else {
+        heart.textContent = EMPTY_HEART
+        heart.className = 'like-glyph'
+      }
+    })
+    .catch(() => {
+      document.getElementById('modal').className = ""
+      setTimeout(() => document.getElementById('modal').className = 'hidden',3000)
+    })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
